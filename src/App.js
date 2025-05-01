30578ac48e7ee8d9f0342b1f9356edf44a0eae06
Sudhesh Hegde <hegdesud2002@gmail.com>
2025-05-01 19:36:53 +0530

    Static waiting page created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-content">
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} className="App-logo" alt="FarmTVStore logo" />
+          <h1>FarmTVStore</h1>
+        </div>
       </header>
+      <main className="App-main">
+        <h2>Under construction, Please wait</h2>
+        <div className="App-download">
+          <p>New to farm tv? Click here to download the App</p>
+          <a href="https://farmtv.shramajeevi.com/" target="_blank" rel="noopener noreferrer">
+            <button className="download-button">Download</button>
+          </a>
+        </div>
+      </main>
     </div>
   );
 }
